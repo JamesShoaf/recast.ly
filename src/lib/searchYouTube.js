def9ee8a79d8323d6e677e8e89ed40b0556feb49
JamesShoaf <james.s.shoaf@gmail.com>
2020-02-19 11:08:00 -0800

    partially implement AJAX call (passes one test)

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,19 @@
-var searchYouTube = (options, callback) => {
+import API_KEY from '../config/youtube.js';
+
+var defaultOptions = {key: API_KEY, query: 'React JS', max: 5};
+
+var searchYouTube = (options = defaultOptions, callback) => {
   // TODO
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: options,
+    success: callback,
+    error: function() {
+      console.error('Failed to get videos');
+    }
+  });
 };
 
 export default searchYouTube;
